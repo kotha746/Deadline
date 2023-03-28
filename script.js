80c9b7cb4f8d0720ff1ce2a94fa1261913997d1f
kotha746 <112921502+kotha746@users.noreply.github.com>
2023-03-28 14:11:28 -0400

    copying from old file

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-// Get the input values
-var startDate = new Date(document.getElementById("startDate").value);
-var endDate = new Date(document.getElementById("endDate").value);
-var today = new Date(document.getElementById("today").value);
-
-// Calculate the remaining days
-var remainingDays = Math.ceil((endDate - today) / (1000 * 60 * 60 * 24));
-
-// Create a chart
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "doughnut",
-  data: {
-    labels: ["Days Remaining", "Days Passed"],
-    datasets: [
-      {
-        label: "Days Left",
-        data: [remainingDays, 365 - remainingDays],
-        backgroundColor: ["#007bff", "#e9ecef"],
-        borderColor: "#ffffff",
-        borderWidth: 2,
-      },
-    ],
-  },
-});
+function createVisualization() {
+    // getitng input values 
+    var startDate = new Date(document.getElementById("startDate").value);
+    var endDate = new Date(document.getElementById("endDate").value);
+    var today = new Date(document.getElementById("today").value);
+  
+    // calculation of days 
+    var remainingDays = Math.ceil((endDate - today) / (1000 * 60 * 60 * 24));
+    var totalDays = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
+  
+    // making the visualization
+    var daysDiv = document.querySelector(".days");
+    daysDiv.innerHTML = "";
+    for (var i = 0; i < totalDays; i++) {
+      var dayDiv = document.createElement("div");
+      dayDiv.classList.add("day");
+      if (i < totalDays - remainingDays) {
+        dayDiv.classList.add("passed");
+      }
+      daysDiv.appendChild(dayDiv);
+    }
+  }
